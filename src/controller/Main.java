b9bc7c5e8d8e06f6883dd990338fd28d8e28bcb7
Omar_Lort <emaranasr@gmail.com>
2019-04-21 15:30:20 +0200

    Opcodes reading and Error checking class

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.ErrorChecker;
 import model.SourceReader;
 
 import java.io.File;
@@ -18,22 +19,25 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Path currentRelativePath = Paths.get("");
-        String path = currentRelativePath.toAbsolutePath().toString() + "\\src\\view\\Editor.fxml";
-        Parent root = FXMLLoader.load(new File(path).toURI().toURL());
-        primaryStage.setTitle("Hello World");
+        //TODO DELETE THE FOLLOWING LINE
+        String path = currentRelativePath.toAbsolutePath().toString() + "/src/view/Editor.fxml";
+
+        Parent root = FXMLLoader.load(new File("src/view/Editor.fxml").toURI().toURL());
+        primaryStage.setTitle("SIC/XE Assembler");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        SourceReader.CommandInfo CI=SourceReader.getInstance().processFile(SourceReader.getInstance().readFile("src/SIC files/SIC_1.txt"));
-        int len=CI.getWholeInstruction().size();
-        for(int i=0;i<len;i++){
-                System.out.println(CI.getWholeInstruction().get(i));
-                System.out.println(CI.getLabel().get(i));
-                System.out.println(CI.getCommand().get(i));
-                System.out.println(CI.getAddressMode().get(i));
-                System.out.println(CI.getOperand1().get(i));
-                System.out.println(CI.getTypeOperand().get(i));
-                System.out.println(CI.getOperand2().get(i));
-        }
+        SourceReader.CommandInfo CI=SourceReader.getInstance().processFile(SourceReader.getInstance().readFile("res/SIC files/SIC_1.txt"));
+       //int len=CI.getWholeInstruction().size();
+       //for(int i=0;i<len;i++){
+       //        System.out.println(CI.getWholeInstruction().get(i));
+       //        System.out.println(CI.getLabel().get(i));
+       //        System.out.println(CI.getCommand().get(i));
+       //        System.out.println(CI.getAddressMode().get(i));
+       //        System.out.println(CI.getOperand1().get(i));
+       //        System.out.println(CI.getTypeOperand().get(i));
+       //        System.out.println(CI.getOperand2().get(i));
+       //}
+        ErrorChecker.getInstance();
     }
 
 
