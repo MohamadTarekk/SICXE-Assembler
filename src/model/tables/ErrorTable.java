36bcbec9b6cf3d2eadb3434184513a0d0247a732
harraz21 <mohamedharraz21@gmail.com>
2019-05-11 03:02:54 +0200

    EQU m3mltsh gheerha fe 2om 2lproject we lazem ted5laha ya numeric ya symbol no expressions xD!

--- src/model/tables/ErrorTable.java
+++ src/model/tables/ErrorTable.java
@@ -26,6 +26,7 @@ public class ErrorTable {
 	public static final int MORE_THAN_ONE_START = 21;
 	public static final int INCORRECT_OPERAND_FORMAT = 22;
 	public static final int OPERAND_EXCEEDED_NUMBER_OF_DECIMAL_DIGITS = 23;
+	public static final int STATEMENT_MUST_HAVE_LABEL= 24;
 
 	public static final int NO_ERROR = 90;
 
@@ -56,6 +57,7 @@ public class ErrorTable {
 		errorList[MORE_THAN_ONE_START] = "ERROR: More that one START encountered";
 		errorList[INCORRECT_OPERAND_FORMAT] = "ERROR: Incorrect Operand";
 		errorList[OPERAND_EXCEEDED_NUMBER_OF_DECIMAL_DIGITS] = "ERROR: Operand exceeded number of decimal digits allowed";
+		errorList[STATEMENT_MUST_HAVE_LABEL]="ERROR: this statement requires a label";
 
 		errorList[NO_ERROR] = "";
 	}
